1372eea15829a92bd90837039d5e597aae7ac8c6
Wayne Patterson <suprsidr@gmail.com>
2020-10-25 13:15:39 -0400

    try react-masonry-css instead of rolling my own

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,12 +1,22 @@
 import React, { useContext } from 'react';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
+import Masonry from 'react-masonry-css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import { AppContext } from './Provider';
 import { download } from '../services/download-service';
 
+import './masonry.scss';
+
 const ImageGrid = ({ query }) => {
   const { state, fetchMore } = useContext(AppContext);
+
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
 
   if (state.photoItems.length === 0) {
     const { page, perPage } = state;
@@ -14,19 +24,22 @@ const ImageGrid = ({ query }) => {
   }
 
   return (
-    <div style={{
-      columns: '6 200px',
-      columnGap: '1rem'
-    }}>
-      {state.photoItems.map(({ id, created, description, altDescription, urls, links, likes, user }) => (
-        <div key={id}>
-          <a href={links.download} onClick={async e => await download(e, id)}>
-            <Image src={urls.thumb} alt={description || altDescription || 'No description'} thumbnail />
-          </a>
-        </div>
-      ))}
-    </div>
-
+    <Row>
+      <Col>
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column">
+          {state.photoItems.map(({ id, created, description, altDescription, urls, links, likes, user }) => (
+            <div key={id}>
+              <a href={links.download} onClick={async e => await download(e, id)}>
+                <Image src={urls.thumb} alt={description || altDescription || 'No description'} thumbnail />
+              </a>
+            </div>
+          ))}
+        </Masonry>
+      </Col>
+    </Row>
   );
 }
 
