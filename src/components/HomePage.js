e836e3820e6403d8cd14573bf8adb405abea1198
Wayne Patterson <suprsidr@gmail.com>
2020-10-25 12:07:04 -0400

    fun up the homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,23 +2,39 @@ import React, { useContext } from 'react';
 import { navigate } from 'hookrouter';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import Image from 'react-bootstrap/Image';
-import { AppContext } from './Provider';
+import Image from 'react-bootstrap/Image';
+import { AppContext, initialState } from './Provider';
 
 const HomePage = () => {
-  // const { state, setState } = useContext(AppContext);
+  const { setState } = useContext(AppContext);
 
   const goto = (e, where) => {
     e.preventDefault();
+    setState(initialState);
     navigate(where);
   }
 
   return (
-    <Row>
-      <Col className="text-center">
-        <h2>Are you a <a href="/cats" onClick={(e) => goto(e, '/cats')}>cat person</a> or a <a href="/dogs" onClick={(e) => goto(e, '/dogs')}>dog person</a>?</h2>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col className="text-center">
+          <h1>Welcome to cats or dogs.</h1>
+          <h2>Make your choice!</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <a href="/cats" onClick={(e) => goto(e, '/cats')}>
+            <Image src="/cat.png" alt="cat" />
+          </a>
+        </Col>
+        <Col className="text-center">
+          <a href="/dogs" onClick={(e) => goto(e, '/dogs')}>
+            <Image src="/dog.png" alt="dog" />
+          </a>
+        </Col>
+      </Row>
+    </>
   )
 }
 
