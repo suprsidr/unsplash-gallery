27b95bca7ec863ffd3bafdea10d6daf3c7de81d0
Wayne Patterson <suprsidr@gmail.com>
2020-10-26 11:43:10 -0400

    move fetchMore to more appropriate place

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import propTypes from 'prop-types';
-import { getCollection } from '../services/lambda-service';
 
 export const AppContext = React.createContext();
 
@@ -23,22 +22,11 @@ const Provider = ({ children, appState = initialState }) => {
     });
   };
 
-  const fetchMore = async ({ page, perPage, query }) => {
-    let results = [];
-    const json = await getCollection({ page, perPage, query });
-    if(json.results) {
-      results = json.results.map(({ id, created_at: created, description, alt_description: altDescription, urls, links, likes, user }) =>
-        ({id, created, description, altDescription, urls, links, likes, user}));
-    }
-    updateState({ page, photoItems: [ ...state.photoItems, ...results ] });
-  };
-
   return (
     <AppContext.Provider
       value={{
         state,
-        setState: updateState,
-        fetchMore
+        setState: updateState
       }}
     >
       {children}
