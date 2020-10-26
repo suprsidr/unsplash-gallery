7f78393891b8b1faded44f17d5125dabc5b4e56d
Wayne Patterson <suprsidr@gmail.com>
2020-10-26 14:07:29 -0400

    link back to homepage

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { A } from 'hookrouter';
 
 const NotFound = () => (
   <Container>
     <Row>
       <Col sm={12}>
-        <div id="error">
+        <div id="error" className="text-center">
           <h2>Oops! That page can’t be found.</h2>
           <p>
             It looks like nothing was found at this location. Maybe try one of
-            the links in the menu or press back to go to the previous page.
+            the links on the <A href="/">homePage</A> or press back to go to the previous page.
           </p>
         </div>
       </Col>
