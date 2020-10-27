3d93ef2a9d438060f98ddaa7598bb64c7dd5da7f
Wayne Patterson <suprsidr@gmail.com>
2020-10-27 12:12:06 -0400

    first test

---
+++ src/components/__tests__/App.js
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../App';
+import Provider from '../Provider';
+
+describe('The App component', () => {
+  test('should not crash', () => {
+    const { getByText, getByAltText } = render(
+      <Provider>
+        <App />
+      </Provider>
+    );
+
+    const h1Element = getByText(/welcome/i);
+    expect(h1Element).toBeInTheDocument();
+    const h2Element = getByText(/make/i);
+    expect(h2Element).toBeInTheDocument();
+    const catElement = getByAltText(/cat/i);
+    expect(catElement).toBeInTheDocument();
+    const dogElement = getByAltText(/dog/i);
+    expect(dogElement).toBeInTheDocument();
+  });
+});
