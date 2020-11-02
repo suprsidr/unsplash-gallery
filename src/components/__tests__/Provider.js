2e07fa3fd2a28a47776a9c4c2941919af7bd862a
Wayne Patterson <suprsidr@gmail.com>
2020-11-02 11:29:22 -0500

    remove unused dependency

--- src/components/__tests__/Provider.js
+++ src/components/__tests__/Provider.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
-import React, { useContext } from 'react';
+import React from 'react';
 import { render } from '@testing-library/react';
 import Provider, { initialState, initialPhotoState } from '../Provider';
 import { useRecoilValue } from 'recoil';
