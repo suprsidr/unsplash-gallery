842c5b61edc48b7c8df9f6dc380daba50d8dfe73
Wayne Patterson <suprsidr@gmail.com>
2020-11-17 10:21:48 -0500

    feat:search

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { A } from 'hookrouter';
+import React, { useRef } from 'react';
+import { A, navigate } from 'hookrouter';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import { BsSearch } from 'react-icons/bs';
 
 import './homePage.scss';
 
 const HomePage = () => {
+
+  const searchInput = useRef();
+
+  const onKeyUpHandler = (e) => {
+    if (e.charCode === 13) {
+      if (e.target.value) {
+        navigate(`/collections/${searchInput.current.value}`)
+      }
+    }
+  }
+
+  const onClickHandler = () => {
+    if (searchInput.current.value) {
+      navigate(`/collections/${searchInput.current.value}`)
+    }
+  }
 
   return (
     <>
@@ -27,6 +47,27 @@ const HomePage = () => {
             <Image src="/dog.png" alt="dog" />
           </A>
         </Col>
+      </Row>
+      <Row className="search-block">
+        <Col xs={12} md={3} lg={4}></Col>
+        <Col xs={12} md={6} lg={4} className="text-center">
+          <p>Need more than cats or dogs?</p>
+          <div>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Search for your favorite"
+                aria-label="Search for your favorite"
+                aria-describedby="Search Text"
+                onKeyPress={onKeyUpHandler}
+                ref={searchInput}
+              />
+              <InputGroup.Append>
+                <Button onClick={onClickHandler} variant="outline-warning"><BsSearch /></Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </div>
+        </Col>
+        <Col xs={12} md={3} lg={4}></Col>
       </Row>
     </>
   )
